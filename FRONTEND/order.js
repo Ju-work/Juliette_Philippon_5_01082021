2db308a3732f7858effb0ccfb320d6f2cb44c75c
Ju-work <79198925+Ju-work@users.noreply.github.com>
2021-11-05 10:44:20 +0100

    05-11-21

--- FRONTEND/order.js
+++ FRONTEND/order.js
@@ -1,2 +1,16 @@
-console.log('testdsqdsqdsq')
-console.log(localStorage.getItem('orderID'))
+ console.log('testdsqdsqdsq')
+ console.log(localStorage.getItem('orderID'))
+
+
+
+ //const contactName = JSON.parse(localStorage.getItem('contact'))
+ const confirmation = document.getElementById('order-confirm')
+
+ confirmation.innerHTML +=`<p> Merci ${document.getElementById('firstName')}</p><p>Votre numéro de commande :
+ <span class="num-order"> ${localStorage.getItem('orderID')}</span></p>
+ <p>Orinoco vous remercie pour votre achat !</p>
+ <p>À bientôt !</p>`
+
+    
+
+
